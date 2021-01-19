b818bd4daab054028849fd844a3068557dfdb5ea
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-19 15:22:55 -0500

    Add Employee

--- queries.sql
+++ queries.sql
@@ -54,4 +54,66 @@ CompanyDatabase> select * FROM "Employees";
 +-----------------+----------+-----------------------+-------------+----------------+
 SELECT 11
 
+SHOW COLUMNS FOR ALL EMPLOYEES
+CompanyDatabase> select * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   |
+|-----------------+----------+-----------------------+-------------+----------------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             |
+| Chris P Bacon   | 120000   | Foodie Chef           | 4444        | NO             |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             |
++-----------------+----------+-----------------------+-------------+----------------+
+SELECT 11
+
+SELECT Full Time Employees with Names and Phone Ext
+CompanyDatabase> SELECT "FullName", "Phone Ext" FROM "Employees" WHERE "Is Part Time"='NO';
+
++---------------+-------------+
+| FullName      | Phone Ext   |
+|---------------+-------------|
+| Santa Claus   | 0           |
+| Shady Beaches | 9999        |
+| Paige Turner  | 6565        |
+| Sam Sung      | 5555        |
+| Chris P Bacon | 4444        |
+| Jet I Knight  | 3232        |
+| Rollo Koster  | 5454        |
++---------------+-------------+
+SELECT 7
+
+INSERT New Employee
+CompanyDatabase> INSERT INTO "Employees" ("FullName", "Salary", "Job Position", "Phone Ext", "Is Part Time")
+................ VALUES('Billy Goat', '450', 'Software Developer', '5353', 'YES' );
+INSERT 0 1
+Time: 0.003s
+CompanyDatabase> SELECT * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   |
+|-----------------+----------+-----------------------+-------------+----------------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             |
+| Chris P Bacon   | 120000   | Foodie Chef           | 4444        | NO             |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             |
+| Billy Goat      | 450      | Software Developer    | 5353        | YES            |
++-----------------+----------+-----------------------+-------------+----------------+
+SELECT 12
+
+UPDATE Cooks Salary to 500
+
+
 
