4fa847827a940f6c99e2dd7bdf0620d292e23147
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-19 15:59:07 -0500

    OMG! I CAN COOK DINNER ON TIME TODAY!! Fiished!

--- queries.sql
+++ queries.sql
@@ -114,6 +114,108 @@ CompanyDatabase> SELECT * FROM "Employees";
 SELECT 12
 
 UPDATE Cooks Salary to 500
+CompanyDatabase>
+ UPDATE "Employees"
+ SET "Salary" = '500' WHERE "Job Position" = 'Foodie Chef';
+
+UPDATE 1
+Time: 0.002s
+CompanyDatabase> Select * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   |
+|-----------------+----------+-----------------------+-------------+----------------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             |
+| Billy Goat      | 450      | Software Developer    | 5353        | YES            |
+| Chris P Bacon   | 500      | Foodie Chef           | 4444        | NO             |
++-----------------+----------+-----------------------+-------------+----------------+
+SELECT 12
+Time: 0.009s
 
 
+DELETE LAZY LARRY
+CompanyDatabase> Insert into "Employees" ("FullName", "Salary", "Job Position", "Phone Ext", "Is Part Time")
+................ VALUES('Lazy Larry', '8800', 'Engineer', '5454', 'YES' );
+INSERT 0 1
+Time: 0.003s
+CompanyDatabase> select * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   |
+|-----------------+----------+-----------------------+-------------+----------------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             |
+| Billy Goat      | 450      | Software Developer    | 5353        | YES            |
+| Chris P Bacon   | 500      | Foodie Chef           | 4444        | NO             |
+| Lazy Larry      | 8800     | Engineer              | 5454        | YES            |
++-----------------+----------+-----------------------+-------------+----------------+
+SELECT 13
+CompanyDatabase> DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry';
+You're about to run a destructive command.
 
+Do you want to proceed? (y/n): t
+Your call!
+DELETE 1
+Time: 0.002s
+
+CompanyDatabase> select * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   |
+|-----------------+----------+-----------------------+-------------+----------------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             |
+| Billy Goat      | 450      | Software Developer    | 5353        | YES            |
+| Chris P Bacon   | 500      | Foodie Chef           | 4444        | NO             |
++-----------------+----------+-----------------------+-------------+----------------+
+SELECT 12
+
+ADD A CLOUMN
+Time: 0.003s
+CompanyDatabase> ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
+You're about to run a destructive command.
+Do you want to proceed? (y/n): y
+Your call!
+ALTER TABLE
+Time: 0.003s
+CompanyDatabase> select * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+---------------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   | ParkingSpot   |
+|-----------------+----------+-----------------------+-------------+----------------+---------------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            | <null>        |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             | <null>        |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             | <null>        |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            | <null>        |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             | <null>        |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             | <null>        |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            | <null>        |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            | <null>        |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             | <null>        |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             | <null>        |
+| Billy Goat      | 450      | Software Developer    | 5353        | YES            | <null>        |
+| Chris P Bacon   | 500      | Foodie Chef           | 4444        | NO             | <null>        |
++-----------------+----------+-----------------------+-------------+----------------+---------------+
+SELECT 12
+Time: 0.010s
