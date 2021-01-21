15dac80bb88d141d4210a6194734768bb2c96416
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-21 09:07:28 -0500

    Database now has 4 tables

--- queries.sql
+++ queries.sql
@@ -166,12 +166,7 @@ CompanyDatabase> select * FROM "Employees";
 +-----------------+----------+-----------------------+-------------+----------------+
 SELECT 13
 CompanyDatabase> DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry';
-You're about to run a destructive command.
 
-Do you want to proceed? (y/n): t
-Your call!
-DELETE 1
-Time: 0.002s
 
 CompanyDatabase> select * FROM "Employees";
 +-----------------+----------+-----------------------+-------------+----------------+
@@ -195,11 +190,7 @@ SELECT 12
 ADD A CLOUMN
 Time: 0.003s
 CompanyDatabase> ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
-You're about to run a destructive command.
-Do you want to proceed? (y/n): y
-Your call!
-ALTER TABLE
-Time: 0.003s
+
 CompanyDatabase> select * FROM "Employees";
 +-----------------+----------+-----------------------+-------------+----------------+---------------+
 | FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   | ParkingSpot   |
@@ -219,3 +210,59 @@ CompanyDatabase> select * FROM "Employees";
 +-----------------+----------+-----------------------+-------------+----------------+---------------+
 SELECT 12
 Time: 0.010s
+
+
+
+
+
+WEDNESDAY HOMEWORK
+CREATE TABLE "Departments"("ID" SERIAL PRIMARY KEY, "DepartmentName" Text, "Building" TEXT); 
+CREATE TABLE
+Time: 0.015s
+
+CompanyDatabase> ALTER TABLE "Employees" ADD COLUMN "ID" SERIAL PRIMARY KEY;
+Time: 0.016s
+CompanyDatabase> SELECT * FROM "Employees";
++-----------------+----------+-----------------------+-------------+----------------+---------------+------+
+| FullName        | Salary   | Job Position          | Phone Ext   | Is Part Time   | ParkingSpot   | ID   |
+|-----------------+----------+-----------------------+-------------+----------------+---------------+------|
+| Ding Dong       | 10000    | Greeter               | 1111        | YES            | <null>        | 1    |
+| Santa Claus     | 1000000  | Santa                 | 0           | NO             | <null>        | 2    |
+| Shady Beaches   | 25000    | Travel Agent          | 9999        | NO             | <null>        | 3    |
+| Winnie Pooh     | 500      | Childrens Reader      | 1212        | YES            | <null>        | 4    |
+| Paige Turner    | 80000    | Marketing Coordinator | 6565        | NO             | <null>        | 5    |
+| Sam Sung        | 300000   | Inventor              | 5555        | NO             | <null>        | 6    |
+| Donal Duck      | 65000    | Actor                 | 8888        | YES            | <null>        | 7    |
+| Woodford Beaver | 120000   | Sommeliers            | 3333        | YES            | <null>        | 8    |
+| Jet I Knight    | 150000   | Producer              | 3232        | NO             | <null>        | 9    |
+| Rollo Koster    | 79000    | Engineer              | 5454        | NO             | <null>        | 10   |
+| Billy Goat      | 450      | Software Developer    | 5353        | YES            | <null>        | 11   |
+| Chris P Bacon   | 500      | Foodie Chef           | 4444        | NO             | <null>        | 12   |
++-----------------+----------+-----------------------+-------------+----------------+---------------+------+
+SELECT 12
+
+CREATE TABLE "PRODUCTS"(
+................ "ID" SERIAL PRIMARY KEY, "PRICE" DECIMAL, "NAME" TEXT, "DESCRIPTION" TEXT, "QTYINSTOCK" INT); 
+CREATE TABLE
+
+CompanyDatabase> CREATE TABLE "ORDERS" ( "ID" SERIAL PRIMARY KEY, "ORDER NUMBER" TEXT, "DATE PLACED" DATE, "EMAIL" TEXT);
+CREATE TABLE
+Time: 0.007s
+
+Time: 0.001s
+CompanyDatabase> \dt
++----------+-------------+--------+----------------+
+| Schema   | Name        | Type   | Owner          |
+|----------+-------------+--------+----------------|
+| public   | Departments | table  | erikastigleman |
+| public   | Employees   | table  | erikastigleman |
+| public   | ORDERS      | table  | erikastigleman |
+| public   | PRODUCTS    | table  | erikastigleman |
++----------+-------------+--------+----------------+
+SELECT 4
+
+
+
+
+
+
